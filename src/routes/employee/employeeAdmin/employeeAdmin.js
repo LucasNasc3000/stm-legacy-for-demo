20dc8611e66d07f044c242acdee0aac56b4249fc
lucasNasc <lucasfortunato328@gmail.com>
2025-06-03 18:24:56 -0300

    rota employeeadmin

--- src/routes/employee/employeeAdmin/employeeAdmin.js
+++ src/routes/employee/employeeAdmin/employeeAdmin.js
@@ -5,7 +5,7 @@ import superAdminPermissionMw from '../../../middlewares/superAdminPermissionMw'
 
 const router = new Router();
 
-router.post('/', loginRequired, superAdminPermissionMw, EmployeeAdmin.Store);
+router.post('/', EmployeeAdmin.Store);
 router.put('/:id', loginRequired, superAdminPermissionMw, EmployeeAdmin.Update);
 
 export default router;
