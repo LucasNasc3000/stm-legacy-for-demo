2b1db363772ab245ce9d1c120d269d1b17b358a6
lucasNasc <lucasfortunato328@gmail.com>
2025-06-20 18:13:32 -0300

    buils test 8

--- dist/database/migrations/20250620211245-removing-constraint-unique-email.js
+++ dist/database/migrations/20250620211245-removing-constraint-unique-email.js
@@ -1,10 +1,11 @@
-/** @type {import('sequelize-cli').Migration} */
+"use strict";/** @type {import('sequelize-cli').Migration} */
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.changeColumn('mfaphrases', 'email', {
       type: Sequelize.STRING,
       allowNull: false,
+      unique: false,
     });
   },
 
