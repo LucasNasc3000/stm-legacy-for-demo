a4972a360b77abfae86c3576cbaaf4cb631f5001
lucasNasc <lucasfortunato328@gmail.com>
2025-06-23 13:48:29 -0300

    Revert "recriando tabela mfaphrases"
    This reverts commit a48f2602ac4f8aaac1ff33fe66c70ee0a5690b38.

--- src/database/migrations/20250424203045-mfa-phrases.js
+++ src/database/migrations/20250424203045-mfa-phrases.js
@@ -17,14 +17,6 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      is_valid: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
