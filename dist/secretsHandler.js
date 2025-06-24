d98bdb8bea77845d88ebf419fb17a07c3957e94a
lucasNasc <lucasfortunato328@gmail.com>
2025-06-23 21:13:58 -0300

    build 23062025

--- dist/secretsHandler.js
+++ dist/secretsHandler.js
@@ -9,6 +9,6 @@ var _path = require('path'); var path = _interopRequireWildcard(_path);
 
     return fs.readFileSync(secretsPath, 'utf-8').trim();
   } catch (err) {
-    console.log('Erro de variável(s) de ambiente');
+    console.log(err);
   }
 } exports.default = SecretsHandler;
