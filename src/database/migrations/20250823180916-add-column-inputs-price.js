38ac127633c8d5c0ae67d0b49dc7757ae9d95b86
lucasNasc <lucasfortunato328@gmail.com>
2026-03-20 20:54:34 -0300

    migration minor fix

--- src/database/migrations/20250823180916-add-column-inputs-price.js
+++ src/database/migrations/20250823180916-add-column-inputs-price.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn('inputs', 'price', {
-      type: Sequelize.DECIMAL,
+      type: Sequelize.DECIMAL(10, 2),
       allowNull: false,
     });
   },
